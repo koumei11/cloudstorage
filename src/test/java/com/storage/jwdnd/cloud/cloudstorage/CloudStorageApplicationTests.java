de690fab1e1d782ae3300a8bb9008cd58e4d6b1f
KOMEI KINJO <miketyson40166@gmail.com>
2020-08-16 20:40:35 +0900

    done

--- src/test/java/com/storage/jwdnd/cloud/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/storage/jwdnd/cloud/cloudstorage/CloudStorageApplicationTests.java
@@ -1,15 +1,5 @@
 package com.storage.jwdnd.cloud.cloudstorage;
 
-import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class CloudStorageApplicationTests {
-	Service service;
-
-	@Test
-	public void test() {
-		assertEquals(5, service.returnInt());
-	}
 }
